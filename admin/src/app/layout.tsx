792bde111afa58ec9bbb3cd5b844a6289c82f0e0
Irving Dinh <irving.dinh@gmail.com>
2026-03-18 17:35:17 +0700

    feat: Add dark mode support to admin UI

--- admin/src/app/layout.tsx
+++ admin/src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next';
 import { Geist } from 'next/font/google';
 import React from 'react';
 
+import { ThemeProvider } from '@/components/theme-provider';
 import { TooltipProvider } from '@/components/ui/tooltip';
 import { cn } from '@/lib/utils';
 
@@ -19,9 +20,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={cn('font-sans', geist.variable)}>
+    <html
+      lang="en"
+      className={cn('font-sans', geist.variable)}
+      suppressHydrationWarning
+    >
       <body>
-        <TooltipProvider>{children}</TooltipProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <TooltipProvider>{children}</TooltipProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
--- admin/src/modules/core/components/AppLayout/AppHeader.tsx
+++ admin/src/modules/core/components/AppLayout/AppHeader.tsx
@@ -1,11 +1,11 @@
+import { ModeToggle } from '@/components/mode-toggle';
 import { SidebarTrigger } from '@/components/ui/sidebar';
 
 export const AppHeader = () => {
   return (
-    <header className="flex h-16 shrink-0 items-center gap-2">
-      <div className="flex items-center gap-2 px-4">
-        <SidebarTrigger />
-      </div>
+    <header className="flex h-16 shrink-0 items-center justify-between gap-2 px-4">
+      <SidebarTrigger />
+      <ModeToggle />
     </header>
   );
 };
